0171b44d2e90cb4dc3be7c262864303b77c1f8e8
Kun Chen <chenkun0228@gmail.com>
2022-08-26 22:29:52 -0400

    rename vegas3.jl

--- example/vegas/benchmark1.jl
+++ example/vegas/benchmark1.jl
@@ -1,13 +1,3 @@
-"""
-Benchmark 1/(1-cos(x)*cos(y)*cos(z)) with different codes.
-Exact: 1.3932
-
-After 2e6 total evaluations
-Kristjan's code: 1.391124528441497 +- 0.0011584630917009374
-classic Vegas: 1.39114(48)
-Vegas + hypercube redistribution: 1.39314(15)
-Cuba: 1.3922290364427665 ± 0.0010906210048935345
-"""
 using MCIntegration
 using PyCall
 using Cuba
@@ -20,6 +10,8 @@ end
 function fc(x)
     return 1.0 / (1.0 - cos(x[1] * π) * cos(x[2] * π) * cos(x[3] * π))
 end
+
+# Kristjan's vegas code gives 1.3922290364427665 ± 0.0010906210048935345
 
 # MCIntegration : 1.3961243672238552 ± 0.008180219118720399
 integrate(neval=200000, var=(Continuous(0.0, 1π, alpha=3.0, adapt=true),), dof=[[3,],]) do config
