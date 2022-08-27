4c014be3fa9781ae0107fbf2c4fe43ab6c3155c2
Kun Chen <chenkun0228@gmail.com>
2022-08-26 22:21:03 -0400

    clean up

--- example/vegas/vegas3.jl
+++ example/vegas/vegas3.jl
@@ -11,6 +11,8 @@ function fc(x)
     return 1.0 / (1.0 - cos(x[1] * π) * cos(x[2] * π) * cos(x[3] * π))
 end
 
+# Kristjan's vegas code gives 1.3922290364427665 ± 0.0010906210048935345
+
 # MCIntegration : 1.3961243672238552 ± 0.008180219118720399
 integrate(neval=200000, var=(Continuous(0.0, 1π, alpha=3.0, adapt=true),), dof=[[3,],]) do config
     x = config.var[1]
@@ -19,12 +21,15 @@ end
 
 # classic Vegas : 1.39114(48)
 integ = Vegas.Integrator([[0, 1π], [0, 1π], [0, 1π]])
-result = integ(f, nitn=10, neval=2e5, beta=0.0)
+result = integ(f, nitn=10, neval=2e5, beta=0.0, alpha=0.5)
+println(result)
 
 # Vegas plus (vegas + hypercube) : 1.39314(15)
 integ2 = Vegas.Integrator([[0, 1π], [0, 1π], [0, 1π]])
-result = integ2(f, nitn=10, neval=2e5)
+result = integ2(f, nitn=10, neval=2e5, alpha=0.5)
+println(result)
 
 # Cuba: 1.3922290364427665 ± 0.0010906210048935345
 result = vegas((x, g) -> g[1] = fc(x), 3, maxevals=2e6)
 @time result = vegas((x, g) -> g[1] = fc(x), 3, maxevals=2e6)
+println(result)
